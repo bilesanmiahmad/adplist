ff34106e6c9a6c2b227c3f9f4db61bca95979e3a
Bilesanmi Ahmad <fbilesanmi@gmail.com>
2022-02-01 21:42:14 +0000

    added css

--- adpproject/settings.py
+++ adpproject/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$we4-)$+jtb(#j2=#hmel(__z09%!uzqcv8jk1cl^(@=35(5j4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ec2-18-130-138-4.eu-west-2.compute.amazonaws.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -141,6 +141,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
